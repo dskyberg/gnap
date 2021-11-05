e98d2a15aab9d2aca12c81981d0e9629708c6a1c
dskyberg <davidskyberg@gmail.com>
2021-11-05 10:31:22 -0400

    Added TLS support

--- gnap_as/src/main.rs
+++ gnap_as/src/main.rs
@@ -1,17 +1,14 @@
+use actix_web::{middleware, web, App, HttpServer};
 use dotenv::dotenv;
-use std::env;
-use std::net::SocketAddr;
-use actix_web::{web, middleware, App, HttpServer};
-use pretty_env_logger;
+
 use log::info;
+use pretty_env_logger;
 
 use dao::service::Service;
-
+use gnap_as::{get_ip_addresses, tls_builder};
+mod grant;
 mod handlers;
 mod routes;
-mod utils;
-mod grant;
-
 
 /// Crate main.
 /// The main service needs to be async, in order to leverage async services.
@@ -26,45 +23,35 @@ async fn main() -> std::io::Result<()> {
     // binary is call `r#as`.
     pretty_env_logger::init();
 
-    // This doesn't really havea ny value.  But fun to play with. We chould just
-    // as easily pass the string from env::var into the HttpServer.bind func.
-    let api_address: SocketAddr = env::var("API_ADDRESS")
-        .expect("API_ADDRESS is not set in env")
-        .parse()
-        .expect("API_ADDRESS is invalid");
-
-    // Get the local IP address of the non-loopback interface. This is just for
-    // displaying at startup.
-    let ip = utils::get_machine_ip();
-    info!("Server is running on {:?}.  IP address is {}", api_address, ip);
+    let (api_address, tls_address, ip) = get_ip_addresses();
+    info!(
+        "\nHTTP is running on {:?}\nHTTPS is running on {:?}\nIP address is {}",
+        &api_address, &tls_address, &ip
+    );
 
     // Init the database and cache services
     let dao_service = Service::create().await;
 
     // App::app_data will wrap the app state in an Arc, so it is sharable
-    let app_state =  web::Data::new(dao_service);
+    let app_state = web::Data::new(dao_service);
 
     // Create the actix-web App instance, with middleware and routes.
     let app = move || {
         App::new()
             // Enable app state data, including DB and Cache stuff.
             .app_data(app_state.clone())
-
             // Add each of the router modules.
             .configure(routes::db::routes)
             .configure(routes::well_known::routes)
             .configure(routes::transaction::routes)
-
             // enable logger - always register actix-web Logger middleware last
             .wrap(middleware::Logger::default())
     };
 
     // Start http server with the app
     HttpServer::new(app)
-    .bind(api_address)?
-    .run()
-    .await
+        .bind(api_address)?
+        .bind_openssl(tls_address, tls_builder())?
+        .run()
+        .await
 }
-
-
-
